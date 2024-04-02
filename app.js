7ffbbbc8a5fba3743057ea671effd51981a7a95a
Khurram Ur Rehman <khurram.rehman@hotmail.com>
2024-04-02 17:17:58 +0000

    lesson 7 completed with express, ejs view engine, dynamic content etc

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+
+app.set('view engine', 'ejs');
+
 let path = './views/'
 
 //listen for requests
@@ -10,21 +14,22 @@ let path = './views/'
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>')
-    let filePath = path + 'index.html';
-    res.sendFile(filePath, {root: __dirname});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index', { title: 'Home', blogs: blogs});
 })
 
 app.get('/about', (req, res) => {
-    let filePath = path + 'about.html';
-    res.sendFile(filePath, {root: __dirname});
+    res.render('about', { title: 'About'})
 });
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
-});
+app.get('/blogs/create', (req,res) => {
+    res.render('create', { title: 'Create'});
+})
 
 app.use((req, res) => {
-    let filePath = path + '404.html'
-    res.status(404).sendFile(filePath, {root: __dirname});
+    res.status(404).render('404', { title: '404'});
 })
