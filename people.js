b15c88671a5f8c8533bf45cd94cfe4f45c4d94dd
Khurram Ur Rehman <khurram.rehman@hotmail.com>
2024-04-02 12:32:46 +0000

    lesson 6 express apps

--- people.js
+++
@@ -1,3 +0,0 @@
-const people = require('./test')
-
-console.log(people)
--- test.js
+++
@@ -1,7 +0,0 @@
-const people = ['hello', 'bello', 'yellow', 'dirty', 'fellow']
-const ages = [20,25,40,30,45]
-
-
-module.exports = {
-    people,ages
-}
