3f688b9a79e42235f47c5d9c6e73f4bf127a1528
Khurram Ur Rehman <khurram.rehman@hotmail.com>
2024-04-03 11:00:12 +0000

    lesson 8 middleware, static files etc completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
@@ -10,8 +11,11 @@ app.set('view engine', 'ejs');
 let path = './views/'
 
 //listen for requests
-
 app.listen(3000);
+
+//middleware and statiac files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     const blogs = [
