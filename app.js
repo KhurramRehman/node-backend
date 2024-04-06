6b1ae9dd2f7042eff63180c48576383ebd0c1b47
KhurramRehman <khurram.rehman@hotmail.com>
2024-04-06 12:06:57 +0100

    lesson 9 completed, mongo db express etc

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //express app
 const app = express();
 
 //connect to mongodb
-const dbURI = 'mongodb+srv://<netninja>:<Mongo787372>@nodetuts.7pi0qfq.mongodb.net/?retryWrites=true&w=majority&appName=nodetuts';
+const dbURI = 'mongodb+srv://netninja:Mongo787372@nodetuts.7pi0qfq.mongodb.net/node-tuts?retryWrites=false&w=majority&appName=nodetuts';
 mongoose.connect(dbURI)
-.then((result) => {
-
-    console.log('connected to db');
-})
-.catch((err) => {
-    console.log(err);
-});
+    .then((result) => {
+        //listen for requests
+        app.listen(3000);
+        console.log('connected to db');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 //register view engine
-
 app.set('view engine', 'ejs');
 
 let path = './views/'
-
-//listen for requests
-app.listen(3000);
 
 //middleware and statiac files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', { title: 'Home', blogs: blogs});
+    res.redirect('/blogs');
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+    .then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result});
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'About'})
+    res.render('about', { title: 'About' })
 });
 
-app.get('/blogs/create', (req,res) => {
-    res.render('create', { title: 'Create'});
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
 })
 
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404'});
+    res.status(404).render('404', { title: '404' });
 })
